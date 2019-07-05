fc4644d6dda33833a22dfda0a00d4dbc441f461b
James Gillispie <james.gillispie@gmail.com>
2019-07-05 19:55:08 -0400

    About

--- src/js/cms-preview-templates/about.js
+++ src/js/cms-preview-templates/about.js
@@ -1,27 +1,25 @@
 import React from "react";
-
-const AboutEntry = ({heading, text}) =>
-  <div>
-    <h4 className="f4 b lh-title mb2 primary">{ heading }</h4>
-    <p>{ text }</p>
-  </div>;
-
-const AboutEntries = ({data}) => data && data.length > 0
-    ? <div className="flex-ns mb3">
-      {data.map(({heading, text}) => <AboutEntry heading={heading} text={text} />)}
-    </div>
-    : "";
+import format from "date-fns/format";
 
 export default class AboutPreview extends React.Component {
   render() {
-    const {entry, getAsset, widgetFor} = this.props;
-    const entryAboutEntries = entry.getIn(["data", "About_entries"]);
-    const AboutEntries = entryAboutEntries ? entryAboutEntries.toJS() : [];
-    return <div className="ph3 bg-off-white">
-      <img src={getAsset(entry.getIn(["data", "logo"]))} alt="" className="db w4 center pv4" />
-      <div className="center mw6 pv3">
+    const {entry, widgetFor, getAsset} = this.props;
+    let image = getAsset(entry.getIn(["data", "image"]));
+
+    return <div className="mw6 center ph3 pv4">
+      <h1 className="f2 lh-title b mb3">{ entry.getIn(["data", "title"])}</h1>
+      <div className="flex justify-between grey-3">
+        <div style={{
+          width: "80px",
+          height: "80px"
+        }}></div>
+        <p>{ format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }</p>
+        <p>Read in x minutes</p>
+      </div>
+      <div className="cms mw6">
+        <p>{ entry.getIn(["data", "description"]) }</p>
+        { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
         { widgetFor("body") }
-        <AboutEntries data={AboutEntries} />
       </div>
     </div>;
   }
